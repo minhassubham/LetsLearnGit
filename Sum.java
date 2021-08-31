4a7dd585757e30f6505947f1b5d702fa4cb77c9c
Subham Minhas <subham.minhas@xaviers.edu.in>
2021-08-31 17:36:06 +0530

    Added Test.txt and modified Sum.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 package LetsLearnGit;
 
 public class Sum {
-    static int a=2;
+    static int a=6;
     static int b=4;
     public static void main(String[] args) {
         System.out.println(a+b);
